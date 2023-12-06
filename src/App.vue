5782f1c0cc0576c424cd30d6f00b453c46f2a2a0
NoahOnFyre <noahonfyre@gmail.com>
2023-12-06 21:50:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Welcome to example website</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, commodi consectetur debitis distinctio dolores ex, maxime modi molestias nam odit pariatur quaerat vero. Fuga nihil nisi possimus ratione temporibus.</p>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+      "./src/*.vue",
+      "./src/components/*.vue",
+      "./src/components/**/*.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
